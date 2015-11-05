09206f9b42bf0c8b748d640a6d11c90afe327d48
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-11-05 14:05:41 +0100

    program structure started

--- test_ising_metropolis/main.cpp
+++ test_ising_metropolis/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <armadillo>
 
 using namespace std;
+using namespace arma;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    double T = 1.0;
+    int L = 2.0;
+
+    maximum_nr_of_cycles = 100000;
+
+
+    //this can be calculated when needed
+    int N = L*L;
+    //microstates
+
+
+    //initial state decided here, random or ordered
+
+
     return 0;
 }
 
--- test_ising_metropolis/test_ising_metropolis.pro
+++ test_ising_metropolis/test_ising_metropolis.pro
@@ -5,3 +5,7 @@ CONFIG -= qt
 
 SOURCES += main.cpp
 
+INCLUDEPATH += /usr/local/include
+LIBS += -L/usr/local/lib
+
+LIBS += -larmadillo -llapack -lblas
