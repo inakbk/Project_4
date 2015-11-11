cb6ae4141732f2a5a9668be2364adbd87b92e752
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-11-11 20:06:22 +0100

    trying to copy all code to new project

---
+++ probability_ising_metropolist/metropolis.h
@@ -0,0 +1,22 @@
+#ifndef METROPOLIS_H
+#define METROPOLIS_H
+#include <armadillo>
+#include "random.h"
+#include <string>
+
+using namespace arma;
+
+//periodic bond. cond.
+inline int periodic(int i, int limit, int add)
+{ return (i+limit+add) % (limit);}
+
+void initialState(Random &random_init_nr, mat &initial_state, int &energy, int &magnetization, int &L, int chosen_initial_state);
+
+void oneFlip(Random &random_nr, mat &spin_state, int &energy, int &magnetization, double T, int L, vec w, int &number_of_accepted_cycles);
+
+void allMCcycles(Random &random_nr, mat &spin_state, int &E, int &M, double T, int L, vec w, int maximum_nr_of_cycles, int chosen_initial_state, int Tcount);
+
+//theoretical values for L=2:
+void theoreticalValues(double T, int chosen_initial_state);
+
+#endif // METROPOLIS_H
