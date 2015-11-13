febefbaf8c4422b11a88fcea660340778e7c1d7f
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-11-13 14:57:01 +0100

    uups, should copy but moved file to another folder

---
+++ equilibrium_ising_metropolis/run_plotT.py
@@ -0,0 +1,197 @@
+from pylab import *
+import os as os
+import matplotlib.pyplot as plt
+
+
+"""
+------------------------------------------------------------------------------------------
+"""
+def exp_E_theory(T, N):
+    return -8.*sinh(8./T)/(cosh(8./T) + 3)/N
+
+def exp_E2_theory(T, N):
+    return 64.*cosh(8./T)/(cosh(8./T) + 3)/N
+
+def C_v_theory(T, N):
+    return ( 64./(T*T) )*( 1 + 3*cosh(8./T) )/( (cosh(8./T) + 3)*(cosh(8./T) + 3) )/N
+
+def exp_absM_theory(T, N):
+    return 2.*(exp(8./T) + 2)/(cosh(8./T) + 3)/N
+
+def exp_M2_theory(T, N):
+    return 8*(exp(8./T) + 1)/(cosh(8./T) + 3)/N
+
+def chi_theory(T, N):
+    #return (8./T)*(exp(8./T) + 1)/(cosh(8./T) + 3) #this one is only working for small L
+    return (4./T)*( ( 2*(exp(8./T) + 1)*(cosh(8./T) + 3) - (exp(8./T) + 2)**2 )/( cosh(8./T) + 3 )**2 )/N
+
+def read_file(filename):
+    infile = open(filename, "r")
+    all_lines = infile.readlines()
+    infile.close()
+
+    nr_of_cycles = zeros(len(all_lines)/10)
+    nr_of_accepted_config = zeros(len(all_lines)/10)
+    mean_E = zeros(len(all_lines)/10)
+    mean_E2 = zeros(len(all_lines)/10)
+    C_v = zeros(len(all_lines)/10)
+    mean_absM = zeros(len(all_lines)/10)
+    mean_M2 = zeros(len(all_lines)/10)
+    chi = zeros(len(all_lines)/10)
+
+    i = 0
+    j = 0
+    while j < len(all_lines)/10:
+        nr_of_cycles[j] = all_lines[0+i].split()[1]
+        nr_of_accepted_config[j] = all_lines[1+i].split()[1]
+
+        mean_E[j] = all_lines[2+i].split()[1]
+        mean_E2[j] = all_lines[3+i].split()[1]
+        C_v[j] = all_lines[4+i].split()[1]
+
+        mean_absM[j] = all_lines[5+i].split()[1]
+        mean_M2[j] = all_lines[6+i].split()[1]
+        chi[j] = all_lines[7+i].split()[1]
+        #T = all_lines[8+i].split()[1]
+        #--- on all_lines[9+i].split()[1]
+        i += 10
+        j += 1
+
+    return nr_of_cycles, nr_of_accepted_config, mean_E, mean_E2, C_v, mean_absM, mean_M2, chi
+
+"""
+------------------------------------------------------------------------------------------
+"""
+
+T = linspace(1,9,20)
+
+L = 2
+N = L**2
+max_nr_of_cycles = 500000 #must delelig 10
+initial = 1
+error_plot = False
+
+#compiling once:
+#os.system('g++ -o main *.cpp -larmadillo -llapack -lblas -L/usr/local/lib -I/usr/local/include -O3 -std=c++11')
+
+nr_of_accepted_config_plot = zeros(len(T))
+mean_E_plot = zeros(len(T))
+mean_E2_plot = zeros(len(T))
+C_v_plot = zeros(len(T))
+mean_absM_plot = zeros(len(T))
+mean_M2_plot = zeros(len(T))
+chi_plot = zeros(len(T))
+
+Tcount = 0
+for i in range(len(T)):
+    #os.system('./main %s %s %s %s %s' %(T[i], L, max_nr_of_cycles, initial, Tcount))
+    filename = 'metropolis_L%s_Tcount%s_initial%s_MC%s.txt' %(L, Tcount, initial, max_nr_of_cycles)
+    cycles, nr_of_accepted_config, mean_E, mean_E2, C_v, mean_absM, mean_M2, chi = read_file(filename)
+    Tcount += 1    
+
+    nr_of_accepted_config_plot[i] = nr_of_accepted_config[-1]
+    mean_E_plot[i] = mean_E[-1]
+    mean_E2_plot[i] = mean_E2[-1]
+    C_v_plot[i] = C_v[-1]
+    mean_absM_plot[i] = mean_absM[-1]
+    mean_M2_plot[i] = mean_M2[-1]
+    chi_plot[i] = chi[-1]
+
+#plot against T:
+#Everything is per spin!
+
+if error_plot == False:
+    figure(1)
+    subplot(3,1,1)
+    plot(T, mean_E_plot)
+    hold('on')
+    plot(T, exp_E_theory(T,N))
+    title('#MCcycles= %g, L= %s, initial_state=%s' %(max_nr_of_cycles, L, initial), fontsize=16)
+    legend(['numerical', 'theory'], fontsize=14, loc='lower right')
+    ylabel('$<E>/J$', fontsize=18)
+
+    subplot(3,1,2)
+    plot(T, mean_E2_plot)
+    hold('on')
+    plot(T, exp_E2_theory(T,N))
+    legend(['numerical', 'theory'], fontsize=14)
+    ylabel('$<E^2>/J^2$', fontsize=18)
+
+    subplot(3,1,3)
+    plot(T, C_v_plot)
+    hold('on')
+    plot(T, C_v_theory(T,N))
+    legend(['numerical', 'theory'], fontsize=14)
+    xlabel('$k_bT$', fontsize=18)
+    ylabel('$C_v/Jk_b$', fontsize=18)
+
+    figure(2)
+    subplot(3,1,1)
+    plot(T, mean_absM_plot)
+    hold('on')
+    plot(T, exp_absM_theory(T,N))
+    title('#MCcycles= %g, L= %s, initial_state= %s' %(max_nr_of_cycles, L, initial), fontsize=16)
+    legend(['numerical', 'theory'], fontsize=14)
+    ylabel('$<|\mathcal{M}|>$', fontsize=18)
+
+    subplot(3,1,2)
+    plot(T, mean_M2_plot)
+    hold('on')
+    plot(T, exp_M2_theory(T,N))
+    legend(['numerical', 'theory'], fontsize=14)
+    ylabel('$<\mathcal{M}^2>$', fontsize=18)
+
+    subplot(3,1,3)
+    plot(T, chi_plot)
+    hold('on')
+    plot(T, chi_theory(T,N))
+    legend(['numerical', 'theory'], fontsize=14)
+    xlabel('$k_bT$', fontsize=18)
+    ylabel('$\chi$', fontsize=18)
+
+if error_plot ==True:
+    figure(1)
+    plot(T, abs(mean_E_plot - exp_E_theory(T,N)))
+    title('Plot of error mean_E\n #MCcycles= %s, L= %s, initial_state=%s' %(max_nr_of_cycles, L, initial))
+    xlabel('T')
+    ylabel('error')
+
+    figure(2)
+    plot(T, abs(mean_E2_plot - exp_E2_theory(T,N)))
+    title('Plot of error mean_E2\n #MCcycles= %s, L= %s, initial_state=%s' %(max_nr_of_cycles, L, initial))
+    xlabel('T')
+    ylabel('error')
+
+    figure(3)
+    plot(T, abs(C_v_plot - C_v_theory(T,N)))
+    title('Plot of error C_v\n #MCcycles= %s, L= %s, initial_state=%s' %(max_nr_of_cycles, L, initial))
+    xlabel('T')
+    ylabel('error')
+
+    figure(4)
+    plot(T, abs(mean_absM_plot - exp_absM_theory(T,N)))
+    title('Plot of error mean_absM\n #MCcycles= %s, L= %s, initial_state=%s' %(max_nr_of_cycles, L, initial))
+    xlabel('T')
+    ylabel('error')
+
+    figure(5)
+    plot(T, abs(mean_M2_plot - exp_M2_theory(T,N)))
+    title('Plot of error mean_M2\n #MCcycles= %s, L= %s, initial_state=%s' %(max_nr_of_cycles, L, initial))
+    xlabel('T')
+    ylabel('error')
+
+    figure(6)
+    plot(T, abs(chi_plot - chi_theory(T,N)))
+    title('Plot of error chi\n #MCcycles= %s, L= %s, initial_state=%s' %(max_nr_of_cycles, L, initial))
+    xlabel('T')
+    ylabel('error')
+
+show()
+
+
+
+
+
+
+
+
